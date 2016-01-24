3bd7a97c4852028574d35098f5168ada7edd652f
Peder Stray <peder.stray@gmail.com>
2016-01-24 11:24:21 +0100

    Fix setup.sh so it does what it should
        Remove cd (which makes the rest of the script fail)
        Remove space around = and replace `` with $()

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@ apt-get update
 apt-get install  libasound2-dev memcached python-pip mpg123 python-alsaaudio
 pip install -r requirements.txt
 cp initd_alexa.sh /etc/init.d/alexa
-cd /etc/rc5.d
-ln -s ../init.d/alexa S99alexa
+ln -s ../init.d/alexa /etc/rc5.d/S99alexa
 touch /var/log/alexa.log
 
 echo "Enter your ProductID:"
@@ -28,7 +27,7 @@ echo "Enter your Security Client Secret:"
 read secret
 echo Client_Secret = \"$secret\" >> creds.py
 
-ip = `ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
+ip=$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
 python ./auth_web.py 
 echo "Open http://$ip:5000"
 
